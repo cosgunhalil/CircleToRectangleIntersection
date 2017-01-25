462b0ccc6ac829142d8de167f368d0dbe5240542
halil cosgun <cosgun.halil@gmail.com>
2017-01-26 01:25:25 +0300

    Circle class is completed

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour {
+
+    private Transform _transform;
+    private float _radius;
+
+    public Circle(Vector2 position , float r)
+    {
+        InitCircle();
+        _transform.position = position;
+        _radius = r;
+    }
+
+    private void InitCircle()
+    {
+        _transform = GetComponent<Transform>();
+    }
+
+    public Vector2 GetCirclePosition()
+    {
+        return _transform.position;
+    }
+
+    public void SetPosition(Vector2 position)
+    {
+        _transform.position = position;
+    }
+}
